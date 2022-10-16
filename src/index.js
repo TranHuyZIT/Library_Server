f22185ade8b546658486d00f3f526e43cd11a34a
TranHuy2k2 <giahuytran2002@gmail.com>
2022-10-16 23:19:43 +0700

    Fixed deployment

--- src/index.js
+++ src/index.js
@@ -18,15 +18,16 @@ mongoose.connect(process.env.DB_URL, () => {
   console.log("MongoDB Connected");
 });
 // Routes
-app.use("/v1/auth", authRoute);
-app.use("/v1/users", userRoute);
-app.use("/v1/books", bookRoute);
-app.use("/v1/orders", orderRoute);
 app.all("/", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
+app.use("/v1/auth", authRoute);
+app.use("/v1/users", userRoute);
+app.use("/v1/books", bookRoute);
+app.use("/v1/orders", orderRoute);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
