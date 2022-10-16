789f9fda27a9c6e418c1a18156335f8647527c0a
TranHuy2k2 <giahuytran2002@gmail.com>
2022-10-16 22:57:39 +0700

    enabled CORS

--- src/index.js
+++ src/index.js
@@ -22,7 +22,11 @@ app.use("/v1/auth", authRoute);
 app.use("/v1/users", userRoute);
 app.use("/v1/books", bookRoute);
 app.use("/v1/orders", orderRoute);
-
+app.all("/", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
